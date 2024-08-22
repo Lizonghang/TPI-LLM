89eefe4bb9c72d5e66b68970375d52ca9a87b0b6
Lizonghang <870644199@qq.com>
2024-08-22 11:42:26 +0400

    add support for llama-3.1

--- src/tpi_llm/split.py
+++ src/tpi_llm/split.py
@@ -1,10 +1,18 @@
 import os
+import re
+import json
 import shutil
-import numpy as np
 import torch
+import numpy as np
+import safetensors.torch
+from tqdm import tqdm
+from collections import defaultdict
 from .modeling_utils import load_model_config
 from .utils import (
     WEIGHTS_NAME,
+    WEIGHTS_INDEX_NAME,
+    SAFE_WEIGHTS_NAME,
+    SAFE_WEIGHTS_INDEX_NAME,
     QKVO_KEY_TEMPLATE,
     MLP_KEY_TEMPLATE,
     ROTARY_EMB_KEY_TEMPLATE,
@@ -19,7 +27,7 @@ from .utils import (
 )
 
 
-def validate_heads_per_node(heads_per_node, num_attention_heads):
+def validate_heads_per_node(heads_per_node, num_heads):
     """
     Validate heads_per_node for correctness.
 
@@ -28,7 +36,7 @@ def validate_heads_per_node(heads_per_node, num_attention_heads):
     - num_attention_heads: Total number of attention heads.
 
     Raises:
-    - ValueError: If any value in heads_per_node is not positive or if the sum does not match num_attention_heads.
+    - ValueError: If any value in heads_per_node is not positive or if the sum does not match num_heads.
     """
     if not all(isinstance(num_heads, int) for num_heads in heads_per_node):
         raise ValueError(f"All values in heads_per_node must be integers, but got {heads_per_node}.")
@@ -36,50 +44,45 @@ def validate_heads_per_node(heads_per_node, num_attention_heads):
     if any(num_heads <= 0 for num_heads in heads_per_node):
         raise ValueError(f"All values in heads_per_node must be positive, but got {heads_per_node}.")
 
-    if sum(heads_per_node) != num_attention_heads:
+    if sum(heads_per_node) != num_heads:
         raise ValueError(
-            f"Sum of heads_per_node {sum(heads_per_node)} does not match num_attention_heads={num_attention_heads}.")
+            f"Sum of heads_per_node {sum(heads_per_node)} does not match num_attention_heads={num_heads}.")
 
 
-def get_heads_per_node(strategy=None, num_attention_heads=None, world_size=None, ratio=None):
+def get_heads_per_node(world_size, ratio, num_heads=None, num_kv_heads=None):
     """
     Calculate the distribution of attention heads across nodes.
 
     Args:
-        strategy (str): Set to "uniform" to allocate attention heads evenly.
-        num_attention_heads (int): The total number of attention heads.
         world_size (int): The number of nodes.
-        ratio (list of float, optional): A list of ratios specifying how attention heads
-            should be distributed across the nodes. This argument is only used if `strategy`
-            is not set to "uniform".
+        ratio (list of float): A list of ratios specifying how attention heads should be
+            distributed across the nodes.
+        num_heads (int, optional): The total number of attention heads.
+        num_kv_heads (int, optional): The total number of key value attention heads.
 
     Returns:
         heads_per_node (list of int): A list where each element represents the number of
             attention heads assigned to each node.
+        kv_heads_per_node (list of int): A list where each element represents the number of
+            key value attention heads assigned to each node.
     """
-    if strategy == "uniform":
-        base_heads = num_attention_heads // world_size
-        remainder = num_attention_heads % world_size
-        heads_per_node = [base_heads] * world_size
-        for node in range(remainder):
-            heads_per_node[node] += 1
-    elif ratio is not None:
-        validate_ratio(ratio, world_size)
-        heads_per_node = [int(num_attention_heads * r) for r in ratio]
-        allocated_heads = sum(heads_per_node)
-        if allocated_heads != num_attention_heads:
-            difference = num_attention_heads - allocated_heads
-            for i in range(abs(difference)):
-                if difference > 0:
-                    heads_per_node[i % world_size] += 1
-                else:
-                    heads_per_node[i % world_size] -= 1
-    else:
-        raise ValueError(f"Invalid strategy {strategy} with ratio=None.")
+    if num_heads is None and num_kv_heads is None:
+        raise ValueError("Either num_heads or num_kv_heads must be specified, but not both.")
+    validate_ratio(ratio, world_size)
 
-    # validate the attention heads distribution.
-    validate_heads_per_node(heads_per_node, num_attention_heads)
-    return heads_per_node
+    def _allocate_heads(num_heads_):
+        heads_per_node_ = [int(num_heads_ * r) for r in ratio]
+        difference_ = num_heads_ - sum(heads_per_node_)
+        for i in range(abs(difference_)):
+            heads_per_node_[i % world_size] += 1 if difference_ > 0 else -1
+        return heads_per_node_
+
+    heads_per_node = _allocate_heads(num_heads) if num_heads is not None else []
+    kv_heads_per_node = _allocate_heads(num_kv_heads) if num_kv_heads is not None else []
+
+    validate_heads_per_node(heads_per_node, num_heads)
+    validate_heads_per_node(kv_heads_per_node, num_kv_heads)
+    return heads_per_node, kv_heads_per_node
 
 
 def validate_ratio(ratio, world_size):
@@ -101,7 +104,50 @@ def validate_ratio(ratio, world_size):
         raise ValueError(f"Ratio values must be positive and sum to 1, but got {ratio} with sum {sum(ratio)}.")
 
 
-def split_attention_heads(weights, layer_num, heads_per_node, head_dim, model_path, save_dir="split"):
+def get_layers_in_sharded_weights(weight_map):
+    """
+    Groups the layer numbers from sharded weight files.
+
+    Args:
+        weight_map (dict): A dictionary where the keys are layer name and the values are
+            the sharded weight filenames.
+
+    Returns:
+        dict: A dictionary where each key is the sharded weight filename, and each value
+            is the set of layer numbers in the sharded weight file.
+    """
+    sharded_layers = defaultdict(set)
+    for layer_name, bin_fn in weight_map.items():
+        match = re.search(r'\d+', layer_name)
+        if match:
+            layer_num = int(match.group())
+            sharded_layers[bin_fn].add(layer_num)
+    return sharded_layers
+
+
+def merge_and_load_weights(sharded_filenames, is_safetensors):
+    """
+    Merges and loads weights from multiple sharded files into a single state_dict.
+
+    Parameters:
+    - sharded_filenames (list of str): List of sharded weight filenames to be merged.
+    - is_safetensors (bool): Whether the sharded files are in the SafeTensors format.
+
+    Returns:
+    - dict: A dictionary containing the merged weights from all sharded files.
+    """
+    merged_weights = {}
+    for filename in sharded_filenames:
+        if is_safetensors:
+            sharded_weights = safetensors.torch.load_file(filename)
+        else:
+            sharded_weights = torch.load(filename)
+        merged_weights.update(sharded_weights)
+        del sharded_weights
+    return merged_weights
+
+
+def split_attention_heads(weights, layer_num, heads_per_node, kv_heads_per_node, head_dim, model_path, save_dir="split"):
     """
     Splits the attention head weights (Q, K, V, O) for a given layer and saves them into separate files
     based on the specified heads_per_node distribution.
@@ -110,6 +156,7 @@ def split_attention_heads(weights, layer_num, heads_per_node, head_dim, model_pa
     - weights: The full model weights dictionary containing all layers.
     - layer_num: The specific layer number to process.
     - heads_per_node: A list specifying the number of attention heads allocated to each node.
+    - kv_heads_per_node: A list specifying the number of key value attention heads allocated to each node.
     - head_dim: The dimension size of each attention head.
     - model_path: The path to the directory where the model files are stored.
     - save_dir: The subdirectory under model_path where the split weights will be saved. Default is "split".
@@ -124,9 +171,10 @@ def split_attention_heads(weights, layer_num, heads_per_node, head_dim, model_pa
 
     # split Q, K, V, O according to heads_per_node
     split_dims = (np.array(heads_per_node) * head_dim).tolist()
+    split_dims_kv = (np.array(kv_heads_per_node) * head_dim).tolist()
     q_slices = weights[q_key].split(split_dims, dim=0)
-    k_slices = weights[k_key].split(split_dims, dim=0)
-    v_slices = weights[v_key].split(split_dims, dim=0)
+    k_slices = weights[k_key].split(split_dims_kv, dim=0)
+    v_slices = weights[v_key].split(split_dims_kv, dim=0)
     o_slices = weights[o_key].split(split_dims, dim=1)
 
     # save q_slices, k_slices, v_slices, o_slices to <save_dir>. For example, for node 1, layer 1,
@@ -137,11 +185,13 @@ def split_attention_heads(weights, layer_num, heads_per_node, head_dim, model_pa
 
         # save q, k, v, o, rotary embedding, and input layernorm
         attn_path = os.path.join(node_dir, ATTN_SAVE_PATH.format(l=layer_num))
-        torch.save({
+        state_dict = {
             input_layernorm_key: weights[input_layernorm_key],
-            rotary_emb_key: weights[rotary_emb_key],
             q_key: q_, k_key: k_, v_key: v_, o_key: o_,
-        }, attn_path)
+        }
+        if rotary_emb_key in weights.keys():
+            state_dict[rotary_emb_key] = weights[rotary_emb_key]
+        torch.save(state_dict, attn_path)
 
 
 def split_mlp(weights, layer_num, heads_per_node, model_path, save_dir="split"):
@@ -200,27 +250,27 @@ def save_input_and_output_weights(weights, model_path, save_dir="split"):
     os.makedirs(node_dir, exist_ok=True)
 
     # save input embedding
-    save_path = os.path.join(node_dir, INPUT_SAVE_PATH)
-    torch.save({INPUT_EMB_KEY: weights[INPUT_EMB_KEY]}, save_path)
+    if INPUT_EMB_KEY in weights.keys():
+        save_path = os.path.join(node_dir, INPUT_SAVE_PATH)
+        torch.save({INPUT_EMB_KEY: weights[INPUT_EMB_KEY]}, save_path)
 
     # save output layernorm and head
-    save_path = os.path.join(node_dir, OUTPUT_SAVE_PATH)
-    torch.save({
-        OUTPUT_LAYERNORM_KEY: weights[OUTPUT_LAYERNORM_KEY],
-        OUTPUT_HEAD_KEY: weights[OUTPUT_HEAD_KEY],
-    }, save_path)
+    if {OUTPUT_LAYERNORM_KEY, OUTPUT_HEAD_KEY}.issubset(set(weights.keys())):
+        save_path = os.path.join(node_dir, OUTPUT_SAVE_PATH)
+        torch.save({
+            OUTPUT_LAYERNORM_KEY: weights[OUTPUT_LAYERNORM_KEY],
+            OUTPUT_HEAD_KEY: weights[OUTPUT_HEAD_KEY],
+        }, save_path)
 
 
-def split_pretrained_model(model_path, world_size, heads_per_node=None, strategy="uniform", ratio=None, save_dir="split"):
+def split_pretrained_model(model_path, world_size, ratio, save_dir="split"):
     """
-    Splits the pretrained model parameters based on the specified strategy.
+    Splits the pretrained model parameters based on the specified ratio.
 
     Parameters:
     - model_path (str): Path to the directory containing the model files.
     - world_size (int): Number of nodes to distribute the attention heads across.
-    - heads_per_node (list of int, optional): A list specifying the number of attention heads per node.
-    - strategy (str, optional): If set to "uniform", allocate attention heads to each node equally.
-    - ratio (list of float, optional): A list specifying the ratio of attention heads to distribute across nodes.
+    - ratio (list of float): A list specifying the ratio of attention heads to distribute across nodes.
     - save_dir: The subdirectory under model_path where the split weights will be saved. Default is "split".
     """
     # Check if save_dir already exists, and delete it if it does
@@ -229,23 +279,42 @@ def split_pretrained_model(model_path, world_size, heads_per_node=None, strategy
     # load model configuration.
     config = load_model_config(model_path)
     num_hidden_layers = config["num_hidden_layers"]
-    num_attention_heads = config["num_attention_heads"]
+    num_heads = config["num_attention_heads"]
+    num_kv_heads = config.get("num_key_value_heads", num_heads)
     hidden_size = config["hidden_size"]
-    head_dim = hidden_size // num_attention_heads
+    head_dim = hidden_size // num_heads
 
-    # allocate attention heads according to heads_per_node, or strategy, or ratio.
-    if heads_per_node is None:
-        heads_per_node = get_heads_per_node(strategy, num_attention_heads, world_size, ratio)
+    # allocate attention heads according to ratio.
+    heads_per_node, kv_heads_per_node = get_heads_per_node(
+        world_size, ratio, num_heads=num_heads, num_kv_heads=num_kv_heads)
 
-    if WEIGHTS_NAME in os.listdir(model_path):
-        # split pretrained model parameters with only one file named "pytorch_model.bin"
-        weight_file = os.path.join(model_path, WEIGHTS_NAME)
-        full_weights = torch.load(weight_file)
-        # save input and output layer on the master node
-        save_input_and_output_weights(full_weights, model_path, save_dir)
-        # split and save attention and mlp layers on each node
-        for layer_num in range(num_hidden_layers):
-            split_attention_heads(full_weights, layer_num, heads_per_node, head_dim, model_path, save_dir)
-            split_mlp(full_weights, layer_num, heads_per_node, model_path, save_dir)
+    bin_file = os.path.join(model_path, WEIGHTS_NAME)
+    bin_index_file = os.path.join(model_path, WEIGHTS_INDEX_NAME)
+    safetensors_file = os.path.join(model_path, SAFE_WEIGHTS_NAME)
+    safetensors_index_file = os.path.join(model_path, SAFE_WEIGHTS_INDEX_NAME)
+    is_safetensors = os.path.exists(safetensors_file) or os.path.exists(safetensors_index_file)
+
+    if os.path.exists(bin_file) or os.path.exists(safetensors_file):
+        # split pretrained model parameters with only one weight file
+        if is_safetensors:
+            full_weights = safetensors.torch.load_file(safetensors_file)
+        else:
+            full_weights = torch.load(bin_file)
+    elif os.path.exists(bin_index_file) or os.path.exists(safetensors_index_file):
+        # merge sharded files first and then split
+        index_file = safetensors_index_file if is_safetensors else bin_index_file
+        with open(index_file, "r") as f:
+            index = json.loads(f.read())
+        sharded_filenames = sorted(set(index["weight_map"].values()))
+        sharded_filenames = [os.path.join(model_path, f) for f in sharded_filenames]
+        full_weights = merge_and_load_weights(sharded_filenames, is_safetensors)
     else:
         raise NotImplementedError("Current weight files are not supported.")
+
+    # save input and output layer on the master node
+    save_input_and_output_weights(full_weights, model_path, save_dir)
+    # split and save attention and mlp layers on each node
+    for layer_num in tqdm(range(num_hidden_layers), desc="Slicing layer", leave=False):
+        split_attention_heads(
+            full_weights, layer_num, heads_per_node, kv_heads_per_node, head_dim, model_path, save_dir)
+        split_mlp(full_weights, layer_num, heads_per_node, model_path, save_dir)
