e278f77c9435574f1019a8644fd307110eb02b3a
Lizonghang <870644199@qq.com>
2024-08-26 08:43:58 +0400

    enable auto model sync

--- examples/run_llama.py
+++ examples/run_llama.py
@@ -1,10 +1,12 @@
 import os
 import logging
 import torch
+import torch.distributed as dist
 from transformers import AutoTokenizer, TextStreamer
 from tpi_llm import TPILlamaForCausalLM
 from tpi_llm.split import split_pretrained_model
 from tpi_llm.modeling_utils import load_model_config
+from tpi_llm.distributed import run_sync_server, download_file
 
 
 logging.basicConfig(
@@ -31,9 +33,42 @@ def adjust_length_to_model(length, max_sequence_length):
 
 
 def main(my_rank, world_size, args):
+    # set random seeds
     torch.manual_seed(args.seed)
     if args.use_gpu:
         torch.cuda.manual_seed_all(args.seed)
+
+    # split and synchronize pretrained model weights
+    args.ratio = [1./args.world_size] * args.world_size  # todo: the decision of ratio should be optimized.
+    split_file_path = os.path.join(args.model_path, args.save_dir)
+    if my_rank == 0:
+        if not os.path.exists(args.model_path):
+            raise Exception(f"Model path {args.model_path} does not exist, "
+                            f"please download the pretrained model parameters first.")
+
+        # split pretrained model files.
+        if not os.path.exists(split_file_path) or args.split_bin:
+            split_pretrained_model(
+                model_path=args.model_path,
+                world_size=world_size,
+                ratio=args.ratio,
+                save_dir=args.save_dir
+            )
+            logger.info(f"All weights are splitted and saved to {split_file_path}.")
+
+        # wait for other nodes to download sliced files.
+        run_sync_server(args.master_ip, args.file_port, args.model_path, split_file_path)
+
+        # ensure that the file download is executed after the master node binds its file port.
+        dist.barrier()
+    else:
+        dist.barrier()
+        # each node download sliced weight files from the master node.
+        if not os.path.exists(split_file_path) or args.force_download:
+            os.makedirs(os.path.join(split_file_path, f"node_{my_rank}"), exist_ok=True)
+            download_file(args.master_ip, args.file_port, my_rank, split_file_path)
+
+    # load model configurations
     model_config = load_model_config(args.model_path)
     max_seq_length = model_config.get("max_position_embeddings", 0)
     args.length = adjust_length_to_model(args.length, max_sequence_length=max_seq_length)
@@ -47,27 +82,6 @@ def main(my_rank, world_size, args):
         model_class, tokenizer_class = MODEL_CLASSES[args.model_type]
     except KeyError:
         raise KeyError(f"Unsupported model type: {args.model_type}")
-
-    # todo: automatically determine ratio
-    args.ratio = [1./args.world_size] * args.world_size
-
-    # split pretrained model files on node with rank 0.
-    if my_rank == 0 and args.split_bin:
-        if not os.path.exists(args.model_path):
-            raise Exception(f"Model path {args.model_path} does not exist, "
-                            f"please download the pretrained model parameters first.")
-
-        split_file_path = os.path.join(args.model_path, args.save_dir)
-        split_pretrained_model(
-            model_path=args.model_path,
-            world_size=world_size,
-            ratio=args.ratio,
-            save_dir=args.save_dir
-        )
-        logger.info(f"All weights are splitted and saved to {split_file_path}.")
-    else:
-        os.makedirs(args.model_path, exist_ok=True)
-        # todo: each node download sliced weight files from the master node.
 
     # load tokenizer and encode user prompt
     tokenizer, streamer = None, None
