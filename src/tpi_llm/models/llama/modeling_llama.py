7f86ab7ad75848e0ab7ec7acb3fa82ab1c56d76f
Lizonghang <870644199@qq.com>
2024-09-04 11:17:43 +0400

    add attn impl for cpu

--- src/tpi_llm/models/llama/modeling_llama.py
+++ src/tpi_llm/models/llama/modeling_llama.py
@@ -1,4 +1,7 @@
+import math
 import argparse
+import time
+
 import torch
 import numpy as np
 from torch import nn
@@ -145,6 +148,57 @@ class TPILlamaAttention(nn.Module):
 
         self.rotary_emb = LlamaRotaryEmbedding(config=self.config)
 
+    def forward(
+        self,
+        hidden_states: torch.Tensor,
+        attention_mask: Optional[torch.Tensor] = None,
+        past_key_value: Optional[Cache] = None,
+        cache_position: Optional[torch.LongTensor] = None,
+        position_embeddings: Optional[Tuple[torch.Tensor, torch.Tensor]] = None,
+        **kwargs,
+    ) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
+        bsz, q_len, _ = hidden_states.size()
+
+        query_states = self.q_proj(hidden_states)
+        key_states = self.k_proj(hidden_states)
+        value_states = self.v_proj(hidden_states)
+
+        query_states = query_states.view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
+        key_states = key_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
+        value_states = value_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
+
+        cos, sin = position_embeddings
+        query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin)
+
+        cache_kwargs = {"sin": sin, "cos": cos, "cache_position": cache_position}
+        key_states, value_states = past_key_value.update(key_states, value_states, self.layer_idx, cache_kwargs)
+
+        key_states = repeat_kv(key_states, self.num_key_value_groups)
+        value_states = repeat_kv(value_states, self.num_key_value_groups)
+
+        attn_weights = torch.matmul(query_states, key_states.transpose(2, 3)) / math.sqrt(self.head_dim)
+
+        if attention_mask is not None:  # no matter the length, we just slice it
+            causal_mask = attention_mask[:, :, :, : key_states.shape[-2]]
+            attn_weights = attn_weights + causal_mask
+
+        # upcast attention to fp32
+        attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query_states.dtype)
+        attn_output = torch.matmul(attn_weights, value_states)
+
+        if attn_output.size() != (bsz, self.num_heads, q_len, self.head_dim):
+            raise ValueError(
+                f"`attn_output` should be of size {(bsz, self.num_heads, q_len, self.head_dim)}, but is"
+                f" {attn_output.size()}"
+            )
+
+        attn_output = attn_output.transpose(1, 2).contiguous()
+        attn_output = attn_output.view(bsz, q_len, -1)
+
+        attn_output = self.o_proj(attn_output)
+
+        return attn_output, None, past_key_value
+
 
 class TPILlamaSdpaAttention(TPILlamaAttention):
 
@@ -220,6 +274,12 @@ class TPILlamaMLP(nn.Module):
         return self.down_proj(self.act_fn(self.gate_proj(x)) * self.up_proj(x))
 
 
+LLAMA_ATTENTION_CLASSES = {
+    "eager": TPILlamaAttention,
+    "sdpa": TPILlamaSdpaAttention,
+}
+
+
 class TPILlamaDecoderLayer(nn.Module):
 
     def __init__(
@@ -254,7 +314,7 @@ class TPILlamaDecoderLayer(nn.Module):
             num_heads=config.num_attention_heads,
             num_kv_heads=config.num_key_value_heads
         )
-        self.self_attn = TPILlamaSdpaAttention(
+        self.self_attn = LLAMA_ATTENTION_CLASSES["sdpa"](
             config=config,
             layer_idx=layer_idx,
             num_heads=heads_per_node[rank],
@@ -428,6 +488,7 @@ class TPILlamaModel(TPILlamaPreTrainedModel):
         hidden_states = inputs_embeds
         del inputs_embeds
 
+        _ = time.time()
         for decoder_layer in self.layers:
             layer_outputs = decoder_layer(
                 hidden_states,
@@ -442,6 +503,7 @@ class TPILlamaModel(TPILlamaPreTrainedModel):
 
             if use_cache:
                 next_decoder_cache = layer_outputs[1]
+        print(time.time() - _)
 
         if self.rank == 0:
             hidden_states = self.norm(hidden_states)
